4b066143725908778248d0d0a8e85e0fa9ff71ef
ShihabRafi1205 <rafy191205@gmail.com>
2026-03-08 23:47:59 +0200

    made organized js code

--- script/home.js
+++ script/home.js
@@ -6,8 +6,6 @@ const closeBtn = document.getElementById("closed-btn");
 const buttons = document.querySelectorAll(".btn1");
 
 
-count.innerText
-
 const spinnerManage = (status) => {
   if(status==true){
     document.getElementById("spinner").classList.remove("hidden")
@@ -29,21 +27,6 @@ const loadIssues = () => {
     });
 };
 
-// {
-// "id": 1,
-// "title": "Fix navigation menu on mobile devices",
-// "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
-// "status": "open",
-// "labels": [
-// "bug",
-// "help wanted"
-// ],
-// "priority": "high",
-// "author": "john_doe",
-// "assignee": "jane_smith",
-// "createdAt": "2024-01-15T10:30:00Z",
-// "updatedAt": "2024-01-15T10:30:00Z"
-// }
 
 const loadModal = (id) => {
   fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
@@ -122,7 +105,7 @@ const displayIssues = (issues) => {
 
   issues.forEach((issue) => {
     const cardDiv = document.createElement("div");
-    cardDiv.innerHTML = `<div class="card space-y-4 shadow-sm p-3 border-t-3 ${issue.status == "open" ? "border-green-500" : "border-purple-500"} h-full">
+    cardDiv.innerHTML = `<div class="card space-y-4 shadow-sm p-3 border-t-3 ${issue.status == "open" ? "border-green-500" : "border-purple-500"}">
 
         <div class="flex justify-between">
         <div><img src="./assets/Open-Status.png" alt="" /></div>
